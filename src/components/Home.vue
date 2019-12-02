c7831460d82a7768396715aa088d5dfac4a8e8c1
dongchao <765308720@qq.com>
2019-12-02 16:02:27 +0800

    登录分支第一次提交

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="home">
+		Home
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+	#home {
+		height: 100%;
+		background-color: #999;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/base.css'
+//阿里图标库
+import './assets/font/iconfont.css'
 
 //引入elementUI库
 import ElementUI from 'element-ui'
 //elementUI库的样式文件
 import 'element-ui/lib/theme-chalk/index.css';
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
