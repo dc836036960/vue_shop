2a9b085f01378fd853e5b28a7547f66aaa56c9d2
dongchao <765308720@qq.com>
2019-12-02 10:56:31 +0800

    项目第一次提交,基本脚手架配置结束

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//引入elementUI库
+import ElementUI from 'element-ui'
+//elementUI库的样式文件
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 
 new Vue({
